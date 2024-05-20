d8d4f2078bf2de93ea92d3eaad4b1bd8d2a65876
Mickael Mogmi Bidon <mogmi95@gmail.com>
2024-05-20 13:43:08 +0200

    Created the jokes repository interface
    Implementations incoming

--- app/src/main/java/fr/mbidon/lumeenproject/repository/JokeRepository.kt
+++ app/src/main/java/fr/mbidon/lumeenproject/repository/JokeRepository.kt
@@ -1,4 +1,27 @@
 package fr.mbidon.lumeenproject.repository
 
-class JokeRepository {
+import fr.mbidon.lumeenproject.model.Joke
+
+/**
+ * Repository to manage Jokes
+ */
+interface JokeRepository {
+    /**
+     * Request a new joke.
+     *
+     * @return a new joke or null if the network is not available.
+     */
+    fun requestNewJoke(): Joke?
+
+    /**
+     * Get all jokes.
+     *
+     * @return the list of all available jokes.
+     */
+    fun getJokes(): List<Joke>
+
+    /**
+     * Deletes any local storage of jokes.
+     */
+    fun clear()
 }
